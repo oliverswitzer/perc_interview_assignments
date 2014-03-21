72a82165f7bcd2ad353ae378d9a606316546e389
oliverswitzer <oliverswitzer@gmail.com>
2014-03-21 16:08:44 -0400

    install figaro gem to hide api keys and mailchimp lists. MailChimp model now functional (can subscribe users to lists). Init custom js script for form validations

--- form_validation/app/controllers/pages_controller.rb
+++ form_validation/app/controllers/pages_controller.rb
@@ -1,16 +1,12 @@
 class PagesController < ApplicationController
 
   def email_new
-    puts Dir.pwd
     gibbon = MailChimp.new
-    puts gibbon.members_of("8afaedfb04")
   end
 
   def email_create
     gibbon = MailChimp.new
-    puts gibbon.members_of("8afaedfb04")
-    params[:email]
-    params[:name]
+    gibbon.subscribe_member_to(ENV["MAILCHIMP_LIST"], "Oliver", "Switzer", params[:email])
   end
 
 end
