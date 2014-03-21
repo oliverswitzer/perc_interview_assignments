3fa7dd744181bbea120b987f5ccc3bbfc1816daf
oliverswitzer <oliverswitzer@gmail.com>
2014-03-21 18:40:53 -0400

    changed form to root path for heroku

--- form_validation/app/controllers/pages_controller.rb
+++ form_validation/app/controllers/pages_controller.rb
@@ -9,9 +9,9 @@ class PagesController < ApplicationController
       name_array.each {|name| name.capitalize!}
       gibbon = MailChimp.new
       gibbon.subscribe_member_to(ENV["MAILCHIMP_LIST"], name_array[0], name_array[1], params[:email])
-      redirect_to "/form", flash: { success: true}
+      redirect_to root_path, flash: { success: true}
     rescue Gibbon::MailChimpError
-      redirect_to "/form", flash: { already_subscribed: "You have already been subscribed to the list" }
+      redirect_to root_path, flash: { already_subscribed: "You have already been subscribed to the list" }
     end
   end
 
