6ed8192d533fcf500f307bf1390c32d6f966fb71
oliverswitzer <oliverswitzer@gmail.com>
2014-03-25 14:17:21 -0400

    create parser functions that parse JSON objects for events. Succesfully outputs to csv file

--- mixpanel_api/mixpanel_export.js
+++ mixpanel_api/mixpanel_export.js
@@ -1,107 +1,105 @@
 // mixpanel_export.js
 
 
-// FOR MIXPANEL QUERY SIGNATURE
-// args = all query parameters going to be sent out with the request 
-//        (e.g. api_key, unit, interval, expire, format, etc.) excluding sig.
-
-// args_sorted = sort_args_alphabetically_by_key(args)
-
-// args_concat = join(args_sorted) 
-
-// # Output: api_key=ed0b8ff6cc3fbb37a521b40019915f18event=["pages"]
-// #         expire=1248499222format=jsoninterval=24unit=hour
-
-// sig = md5(args_concat + api_secret)
-
 // node modules
-var http = require('http'),
-    Mixpanel = require('mixpanel'),
-    md5 = require('MD5'),
-    jsoncsv = require('jsoncsv'),
-    fs = require('fs'),
-    stringify = require('csv-stringify');
+http = require('http');
+Mixpanel = require('mixpanel');
+md5 = require('MD5');
+jsoncsv = require('jsoncsv');
+fs = require('fs');
+stringify = require('csv-stringify');
 
+// cmd line input to get the event to filter the request by
+eventFilter = process.argv[2];
 
+// query params needed to make Mixpanel signature
+today = new Date;
+expireUTC = today.getTime() + 1E8;  // 1E8 is approximately a day in milliseconds
+params = ["from_date=2014-01-01", "to_date=2014-03-10", "api_key=6968655dca8ecbf52604b137c6354ed6", "expire=" + expireUTC];
 
+// if the user has supplied an event to filter by, push it to params array 
+if (eventFilter !== undefined) {
+  params.push("event=" + JSON.stringify([eventFilter]))
+}   
 
-// query params needed to make signature
-var today = new Date,
-    expireUTC = today.getTime() + 1E8,  // 1E8 is approximately a day in milliseconds
-    args = ["from_date=2014-01-01", "to_date=2014-03-10", "api_key=6968655dca8ecbf52604b137c6354ed6", "expire=" + expireUTC],
-    argsConcat = args.sort().join(""),
-    signature = md5(argsConcat + "1bd71a4868d41f1dd76b702bff3c3e02");
+// create the signature
+paramsConcat = params.sort().join(""),
+signature = md5(paramsConcat + "1bd71a4868d41f1dd76b702bff3c3e02");
 
+//concat query params 
 var base_url = "http://data.mixpanel.com/api/2.0/export/?",
-    request = base_url + args.join("&") + "&sig=" + signature;
+    request = base_url + params.join("&") + "&sig=" + signature;
 
+console.log("REQUEST URL: " + request)
 
-
+// make get request to Mixpanel API
 var req = http.get(request, function(response) {
   body = '';
   response.on("data", function(data) {
-    body += data.toString();   // getting 'chunks' of json at a time and appending to body message
+    body += data.toString();   // getting 'chunks' of data at a time and appending to body message
   });
   response.on("end", function() {
     /*NOTE: Since mixpanel doesnt output standard json, I had to do some messing around to convert the response into proper JSON (e.g. splitting on return characters) */
-    
     var eventArray = body.split(/\n/); //an array with each event json object as an element
+    
+    // init our arrays for outputting to CSV
     var columns,
         outputToCSV = [];
 
-    console.log(typeof(eventArray[1]));
+    // iterate through array of JSON objects
     for (var i = 0, eventJSON; i < eventArray.length - 1; i++) {
       eventJSON = eventArray[i];
-      var eventObj = JSON.parse(eventJSON)
+      var eventObj = JSON.parse(eventJSON),
+          flatEventObj = flattenObject(eventObj); // a function which flattens nested properties into an object with on level of properties
+
       if (i == 0) {
+        // get columns from first event object
         columns = parseEventColumns(eventObj);
         outputToCSV.push(columns);
       } else {
-        eventValues = parseEventValues(eventObj);
+        // parse each object's values
+        eventValues = parseEventValues(flatEventObj, columns);
         outputToCSV.push(eventValues);
       }
     }
 
-    stringify(outputToCSV, function(err, output){
-      // console.log(output);
-      // fs.writeFile("/tmp/test.csv", "Hey there!", function(err) {
-      //   if(err) {
-      //     console.log(err);
-      //   } else {
-      //     console.log("The file was saved!");
-      //   }
-      // }); 
-    });
+    saveCSV(outputToCSV);
+  
   });
 }).on("error", function(e) {
   console.log("Got error: " + e.message);
 })
 
 
-
-function parseEventValues(obj, columns) {
-  var propValues = [];
-  for (var prop in obj) {
-    if (obj.hasOwnProperty(prop)) {  // to sure we are only iterating through specific objects properties and not its inherited prototype methods 
-      if (typeof(obj[prop]) == "object"){
-        for (var subProp in obj[prop]) {
-          propValues.push(obj[prop][subProp]);
-        }
+var saveCSV = function(outputArray) {
+  stringify(outputArray, function(err, output){
+    fs.writeFile("eventOutput.csv", output, function(err) {
+      if(err) {
+        console.log(err);
       } else {
-        propValues.push(obj[prop])
+        console.log("The file was saved!");
       }
-    }
-  }
-  return propValues
+    }); 
+  });
 }
 
-function parseEventColumns(obj) {
+var parseEventValues = function(obj, columns) {
+  var propValues = [];
+  for (var i in columns) {
+    propValues.push(obj[columns[i]])  
+  }
+  return propValues
+};
+
+var parseEventColumns = function(obj) {
   var propColumns = [];
   for (var prop in obj) {
     if (obj.hasOwnProperty(prop)) {  // to sure we are only iterating through specific objects properties and not its inherited prototype methods 
       if (typeof(obj[prop]) == "object"){
         for (var subProp in obj[prop]) {
-          propColumns.push(subProp);
+          if (typeof(obj[prop][subProp]) != "object") {
+            propColumns.push(subProp);
+          }
         }
       } else {
         propColumns.push(prop)
@@ -109,13 +107,27 @@ function parseEventColumns(obj) {
     }
   }
   return propColumns
-}
+};
 
-
-// input = [ [ '1', '2', '3', '4' ], [ 'a', 'b', 'c', 'd' ] ];
-// stringify(input, function(err, output){
-//   output.should.eql('1,2,3,4\na,b,c,d');
-// });
+var flattenObject = function(ob) {
+  var toReturn = {};
+  
+  for (var i in ob) {
+    if (!ob.hasOwnProperty(i)) continue;
+    
+    if ((typeof ob[i]) == 'object') {
+      var flatObject = flattenObject(ob[i]);
+      for (var x in flatObject) {
+        if (!flatObject.hasOwnProperty(x)) continue;
+        
+        toReturn[x] = flatObject[x];
+      }
+    } else {
+      toReturn[i] = ob[i];
+    }
+  }
+  return toReturn;
+};
 
 
 
